5235a80fa42c1ef3a5d2a72b50aca06b487ecf1a
Michiel Kempen <kempenmichiel@gmail.com>
2022-05-01 23:29:49 +0200

    Test new config

--- templates/laravel/template/nginx/start.sh
+++ templates/laravel/template/nginx/start.sh
@@ -7,18 +7,12 @@ do
   if [[ "$run" -gt "1" ]]; then
     echo "Retrying..."
   fi
-  
-  response=$(
-    SCRIPT_NAME=/fpm-status SCRIPT_FILENAME=/fpm-status REQUEST_METHOD=GET cgi-fcgi -bind -connect 127.0.0.1:9000 || true
-  )
 
-  # if [[ "$response" == *"pool"* ]]; then
-  #   echo "FPM is running and ready. Starting nginx."
-  /usr/sbin/nginx -c /etc/nginx/nginx.conf
-  exit 0
-  # else
-  #   echo "$response"
-  # fi
+  if [[ ! -z "$(netstat -an | grep :9000)" ]]; then
+    echo "FPM is running and ready. Starting nginx."
+    /usr/sbin/nginx -c /etc/nginx/nginx.conf
+    exit 0
+  fi
 
   sleep .5
 done
