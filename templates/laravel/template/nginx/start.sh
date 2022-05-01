e3e029a7516f3b4c0a5f6c0490311603c59ac00d
Michiel Kempen <kempenmichiel@gmail.com>
2022-05-01 22:06:03 +0200

    debug

--- templates/laravel/template/nginx/start.sh
+++ templates/laravel/template/nginx/start.sh
@@ -12,13 +12,13 @@ do
     SCRIPT_NAME=/fpm-status SCRIPT_FILENAME=/fpm-status REQUEST_METHOD=GET cgi-fcgi -bind -connect 127.0.0.1:9000 || true
   )
 
-  if [[ "$response" == *"pool"* ]]; then
-    echo "FPM is running and ready. Starting nginx."
-    /usr/sbin/nginx -c /etc/nginx/nginx.conf
-    exit 0
-  else
-    echo "$response"
-  fi
+  # if [[ "$response" == *"pool"* ]]; then
+  #   echo "FPM is running and ready. Starting nginx."
+  /usr/sbin/nginx -c /etc/nginx/nginx.conf
+  exit 0
+  # else
+  #   echo "$response"
+  # fi
 
   sleep .5
 done
